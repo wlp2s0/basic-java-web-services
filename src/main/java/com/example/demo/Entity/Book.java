9ce6b84f9a167c2e2e3b9f72d962915edfaa964a
Mattia Campagna <mattia.campagna@noku.io>
2020-11-16 17:42:15 +0100

    Fix

--- src/main/java/com/example/demo/Entity/Book.java
+++ src/main/java/com/example/demo/Entity/Book.java
@@ -1,13 +1,17 @@
 package com.example.demo.Entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Book {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
     private Integer id;
     private String title;
     private String author;
