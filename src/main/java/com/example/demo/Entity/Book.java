7cdda7041cd983dbd0ba818e9f79735fadc1f8c1
Simone Romano <simone.romano@noku.io>
2020-11-16 18:12:36 +0100

    fix import

--- src/main/java/com/example/demo/Entity/Book.java
+++ src/main/java/com/example/demo/Entity/Book.java
@@ -3,6 +3,7 @@ package com.example.demo.Entity;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
